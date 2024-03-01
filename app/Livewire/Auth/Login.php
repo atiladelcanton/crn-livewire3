1f65bc0d39a48833d3c574d26166aa4d8b79a4c1
Átila Delcanton Rampazo <atila.rampazo@atila.rampazo>
2024-03-01 16:53:39 -0300

    crn-01: X

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -2,7 +2,8 @@
 
 namespace App\Livewire\Auth;
 
-use Auth;
+use Illuminate\Support\Facades\{Auth, RateLimiter};
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class Login extends Component
@@ -18,8 +19,16 @@ class Login extends Component
 
     public function tryToLogin(): void
     {
+
+        if(RateLimiter::tooManyAttempts(Str::lower($this->email), 5)) {
+            $this->addError('rateLimiter', trans('auth.throttle', []));
+
+            return;
+        }
+
         if (!Auth::attempt(['email' => $this->email, 'password' => $this->password])) {
             $this->addError('invalidCredentials', trans('auth.failed'));
+            RateLimiter::hit(Str::lower($this->email));
 
             return;
         }
