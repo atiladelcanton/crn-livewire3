a7b5ed710e54a38d86d2672c70e25b5c364c2045
Átila Delcanton Rampazo <atila.rampazo@atila.rampazo>
2024-03-04 15:33:23 -0300

    crm-3: testing email property, making sure it is required and is real email

--- tests/Feature/Auth/PasswordRecoveryTest.php
+++ tests/Feature/Auth/PasswordRecoveryTest.php
@@ -2,7 +2,6 @@
 
 use App\Livewire\Auth\Livewire\Recovery;
 use App\Models\User;
-
 use App\Notifications\PasswordRecoveryNotification;
 
 use function Pest\Laravel\get;
@@ -15,10 +14,19 @@ it('should be able to request for password recovery sending notification to the 
     \Illuminate\Support\Facades\Notification::fake();
     /** @var User $user */
     $user = User::factory()->create();
-    Livewire::test(Recovery::class)
-        ->assertDontSee('You will receive an email with the password recovery link')
-        ->set('email', $user->email)
-        ->call('startPasswordRecovery')
-        ->assertSee('You will receive an email with the password recovery link');
+    Livewire::test(Recovery::class)->assertDontSee('You will receive an email with the password recovery link')->set(
+        'email',
+        $user->email
+    )->call('startPasswordRecovery')->assertSee('You will receive an email with the password recovery link');
     \Illuminate\Support\Facades\Notification::assertSentTo($user, PasswordRecoveryNotification::class);
 });
+
+test('making sure the email is real', function ($value, $rule) {
+    Livewire::test(Recovery::class)
+        ->set('email', $value)
+        ->call('startPasswordRecovery')
+        ->assertHasErrors(['email' => $rule]);
+})->with([
+    'required' => ['value' => '', 'rule' => 'required'],
+    'email'    => ['value' => 'any email', 'rule' => 'email'],
+]);
