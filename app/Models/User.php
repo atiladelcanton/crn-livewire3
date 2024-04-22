37b46dfb7ad7eac2090174a17f9dce1e70802cf2
Átila Delcanton Rampazo <atila.rampazo@atila.rampazo>
2024-04-22 14:57:32 -0300

    refactoring permissions to his trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,77 +3,31 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Cache;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens;
     use HasFactory;
+    use HasPermissions;
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password'          => 'hashed',
     ];
-
-    public function permissions(): BelongsToMany
-    {
-        return $this->belongsToMany(Permission::class);
-    }
-    public function givePermissionTo(string $key): void
-    {
-        $this->permissions()->firstOrCreate(compact('key'));
-        Cache::forget($this->permissionCacheKey());
-        Cache::rememberForever($this->permissionCacheKey(), fn () => $this->permissions);
-    }
-
-    public function hasPermissionTo(string $key): bool
-    {
-        /** @var Collection $permissions */
-        $permissions = Cache::get($this->permissionCacheKey(), $this->permissions);
-
-        return $permissions
-            ->where('key', $key)
-            ->isNotEmpty();
-    }
-
-    /**
-     * @return string
-     */
-    private function permissionCacheKey(): string
-    {
-        return "user::{{$this->id}}::permissions";
-    }
 }
