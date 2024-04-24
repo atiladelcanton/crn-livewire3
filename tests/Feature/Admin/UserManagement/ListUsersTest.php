1c75494d08e6b9c66e9595aab3a869e8666e6df4
Átila Delcanton Rampazo <atila.rampazo@atila.rampazo>
2024-04-24 09:32:57 -0300

    making sure that the route is protected by the permission BE_AN_ADMIN

--- tests/Feature/Admin/UserManagement/ListUsersTest.php
+++ tests/Feature/Admin/UserManagement/ListUsersTest.php
@@ -8,3 +8,8 @@ it('should be able to access the route admin/users', function () {
     actingAs(User::factory()->admin()->create());
     get(route('admin.users'))->assertOk();
 });
+
+test('making sure that the route is protected by the permission BE_AN_ADMIN', function () {
+    actingAs(User::factory()->create());
+    get(route('admin.users'))->assertForbidden();
+});
